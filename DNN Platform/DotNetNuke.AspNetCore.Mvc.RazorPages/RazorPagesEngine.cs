24711e83beefbb621236ce59d4e8a2dcb38d3379
Andrew Hoefling <andrew@hoefling.me>
2019-10-14 23:35:58 -0400

    Implemented the RazorPages Engine based on the Proof of Concept

--- DNN Platform/DotNetNuke.AspNetCore.Mvc.RazorPages/RazorPagesEngine.cs
+++ DNN Platform/DotNetNuke.AspNetCore.Mvc.RazorPages/RazorPagesEngine.cs
@@ -1,4 +1,12 @@
 ﻿using DotNetNuke.UI.Modules;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.IO;
 
@@ -6,9 +14,53 @@ namespace DotNetNuke.AspNetCore.Mvc.RazorPages
 {
     internal class RazorPagesEngine : IEngine
     {
+        protected IRazorViewEngine ViewEngine { get; }
+        protected ITempDataProvider TempDataProvider { get; }
+        protected IServiceProvider ServiceProvider { get; }
+
+        public RazorPagesEngine(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            ViewEngine = viewEngine;
+            TempDataProvider = tempDataProvider;
+            ServiceProvider = serviceProvider;
+        }
+
         public void Render(string file, ModuleInstanceContext moduleContext, string localResourceFile, TextWriter writer)
         {
-            throw new NotImplementedException();
+            var actionContext = GetActionContext();
+            var viewEngineResult = ViewEngine.FindView(GetActionContext(), file, false);
+
+            if (!viewEngineResult.Success)
+                throw new InvalidOperationException($"Couldn't find view '{file}'");
+
+            var view = viewEngineResult.View;
+            using (var output = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary()),
+                    new TempDataDictionary(actionContext.HttpContext, TempDataProvider),
+                    output,
+                    new HtmlHelperOptions());
+
+                view.RenderAsync(viewContext).RunSynchronously();
+            }
+        }
+
+        private ActionContext GetActionContext()
+        {
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = ServiceProvider
+            };
+
+            return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
         }
     }
 }
